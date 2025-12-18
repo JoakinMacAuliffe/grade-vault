d45cd6c3cc51dad919325d7b97a87f2e44888b24
JoakinMacAuliffe <jfmacauliffe@gmail.com>
2025-12-18 00:46:07 -0300

    refactor: restructure project layout and components, remove unused files

---
+++ app/layout.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+import Header from '../components/header'
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
