3281eb77f5d8b38d5ff177cead6ca48d77ebcc5e
JoakinMacAuliffe <jfmacauliffe@gmail.com>
2025-12-21 14:34:35 -0300

    feat: add @vercel/analytics dependency and integrate it into the layout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import "./globals.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
 
+import { Analytics } from "@vercel/analytics";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
