408f0c5d7baa6404e38fae6b93bd9ebd6f62863e
JoakinMacAuliffe <jfmacauliffe@gmail.com>
2025-12-23 22:56:43 -0300

    feat: update Analytics import path to use the next version

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
 
-import { Analytics } from "@vercel/analytics";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({ children }) {
   return (
